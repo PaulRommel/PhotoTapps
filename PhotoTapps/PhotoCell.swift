0d0cefd344f752e0a98b6a7eee65e3a8743b9169
Paul <paulrommel87@gmail.com>
2021-04-03 14:23:06 +0300

    Custom CollectionView Cell.

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,14 @@
+ //
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Павел Попов on 30.03.2021.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var starImageView: UIImageView!
+    
+}
